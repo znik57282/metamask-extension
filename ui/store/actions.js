e01b7d33263f74673f35c1ce6ed213616eeb161e
David Walsh <davidwalsh83@gmail.com>
2022-11-15 13:28:50 -0600

    Add missing await

--- ui/store/actions.js
+++ ui/store/actions.js
@@ -3289,7 +3289,7 @@ export function getContractMethodData(data = '') {
     dispatch(loadingMethodDataStarted());
     log.debug(`loadingMethodData`);
 
-    const { name, params } = getMethodDataAsync(fourBytePrefix);
+    const { name, params } = await getMethodDataAsync(fourBytePrefix);
 
     dispatch(loadingMethodDataFinished());
     callBackgroundMethod(
